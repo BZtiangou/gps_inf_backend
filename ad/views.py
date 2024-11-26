60940c823b338008c553c40ccda06963e2f10a9d
Sqrt3 <1273750248@qq.com>
2024-11-26 19:20:11 +0800

    新增对传入图像进行安全检查

--- ad/views.py
+++ ad/views.py
@@ -3,16 +3,49 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
+from PIL import Image
 import subprocess
 import os
 
 class ImagePredictAPIView(APIView):
     permission_classes = []
+
+    # def check_image_header(self, file):
+    #     header = file.read(10)
+    #     file.seek(0)  # 重置文件指针位置
+    #     return (
+    #         header.startswith(b'\xff\xd8\xff') or  # JPEG
+    #         header.startswith(b'\x89PNG\r\n\x1a\n') or  # PNG
+    #         header.startswith(b'GIF87a') or
+    #         header.startswith(b'GIF89a')  # GIF
+    #     )
+
+    def process_image(self, image_file):
+        try:
+            img = Image.open(image_file)
+            img.verify()  # 验证图像
+            img.close()
+        except (IOError, SyntaxError) as e:
+            return False
+        return True
+
     def post(self, request):
         # 获取上传的图片
         image_file = request.FILES.get('file')
         if not image_file:
             return Response({"error": "No image file provided"}, status=status.HTTP_400_BAD_REQUEST)
+
+        # 检查文件MIME类型
+        if image_file.content_type not in ['image/jpeg', 'image/png', 'image/gif']:
+            return Response({"error": "Unsupported file type"}, status=status.HTTP_400_BAD_REQUEST)
+
+        # # 检查图片文件头
+        # if not self.check_image_header(image_file):
+        #     return Response({"error": "File is not a valid image"}, status=status.HTTP_400_BAD_REQUEST)
+
+        # 使用Pillow验证图像
+        # if not self.process_image(image_file):
+        #     return Response({"error": "Invalid image"}, status=status.HTTP_400_BAD_REQUEST)
 
         # 保存图片到 media/picture
         fs = FileSystemStorage(location=settings.MEDIA_ROOT + '/picture')
@@ -35,5 +68,15 @@ class ImagePredictAPIView(APIView):
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-        # 返回预测结果
-        return Response({"prediction": prediction}, status=status.HTTP_200_OK)
+        # 提取表单数据
+        bookname = request.data.get('bookname')
+        author = request.data.get('author')
+        publisher = request.data.get('publisher')
+
+        # 返回预测结果和表单数据
+        return Response({
+            "prediction": prediction,
+            "bookname": bookname,
+            "author": author,
+            "publisher": publisher
+        }, status=status.HTTP_200_OK)
